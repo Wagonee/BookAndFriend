d415ba5fb7f0e80339756044682fb543f9f91d9f
Wagonee <ponomarev.ny@yandex.ru>
2025-06-23 13:16:46 +0300

    base structure added and frameworks included

--- app/src/main/java/com/example/bookandfriend/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bookandfriend/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bookandfriend
+package com.example.bookandfriend.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bookandfriend.ui.theme.BookAndFriendTheme
+import com.example.bookandfriend.presentation.ui.theme.BookAndFriendTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/bookandfriend/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/bookandfriend/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.bookandfriend.ui.theme
+package com.example.bookandfriend.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
