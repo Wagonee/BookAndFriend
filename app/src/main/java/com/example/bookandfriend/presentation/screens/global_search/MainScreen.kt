11a06f7908a212bcf1715e8a7cd6a9a5e5cd33f9
Konstantin <kostyabash@bk.ru>
2025-06-24 01:02:18 +0300

    Application design template created. The logic of working with application settings is connected to visual elements

--- app/src/main/java/com/example/bookandfriend/presentation/screens/global_search/MainScreen.kt
+++ app/src/main/java/com/example/bookandfriend/presentation/screens/global_search/MainScreen.kt
@@ -1,9 +1,43 @@
 package com.example.bookandfriend.presentation.screens.global_search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.bookandfriend.presentation.navigation.BottomBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(
+    navController: NavController,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Book And Friend") }
+            )
+        },
+        bottomBar = {
+            BottomBar(navController = navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
 
+        }
+    }
 }
--- app/src/main/java/com/example/bookandfriend/presentation/screens/library/LibraryScreen.kt
+++ app/src/main/java/com/example/bookandfriend/presentation/screens/library/LibraryScreen.kt
@@ -1,2 +1,42 @@
 package com.example.bookandfriend.presentation.screens.library
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.bookandfriend.presentation.navigation.BottomBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LibraryScreen(
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Favourites") }
+            )
+        },
+        bottomBar = {
+            BottomBar(navController = navController)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+
+        }
+    }
+}
