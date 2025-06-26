02fe2ac055004cd7ead28a1b1d186d73756adc63
Konstantin <kostyabash@bk.ru>
2025-06-26 19:23:38 +0300

    Improved visual style of screens. Added content to the RandomSearchScreen

--- app/src/main/java/com/example/bookandfriend/presentation/screens/global_search/MainScreen.kt
+++ app/src/main/java/com/example/bookandfriend/presentation/screens/global_search/MainScreen.kt
@@ -1,29 +1,54 @@
 package com.example.bookandfriend.presentation.screens.global_search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bookandfriend.presentation.navigation.BottomBar
+import com.example.bookandfriend.presentation.ui.theme.LocalCustomColors
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     navController: NavController,
 ) {
+    val customColors = LocalCustomColors.current
+
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("Book And Friend") }
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Book and Friend",
+                        textAlign = TextAlign.Center
+                    )
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = customColors.background,
+                    titleContentColor = customColors.textColor
+                ),
+                modifier = Modifier.shadow(
+                    elevation = 8.dp,
+                    shape = RectangleShape,
+                    ambientColor = customColors.shadowColor,
+                    spotColor = customColors.shadowColor
+                )
             )
         },
         bottomBar = {
@@ -33,9 +58,11 @@ fun MainScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(customColors.background)
                 .padding(innerPadding)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(24.dp)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
         }
