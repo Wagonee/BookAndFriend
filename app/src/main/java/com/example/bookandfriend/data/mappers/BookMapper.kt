d278f0df6802ab4c9ea45176d8adc8bb5e5126b5
Wagonee <ponomarev.ny@yandex.ru>
2025-06-26 13:18:11 +0300

    network layer fixes and TestActivity.kt added

--- app/src/main/java/com/example/bookandfriend/data/mappers/BookMapper.kt
+++ app/src/main/java/com/example/bookandfriend/data/mappers/BookMapper.kt
@@ -4,24 +4,34 @@ import com.example.bookandfriend.data.network.dto.BookDetailsDto
 import com.example.bookandfriend.data.network.dto.BookDto
 import com.example.bookandfriend.domain.model.Book
 import com.example.bookandfriend.domain.model.BookDetails
+import com.google.gson.internal.LinkedTreeMap
 
 class BookMapper {
     fun mapDtoToDomain(dto: BookDto): Book {
         return Book(
             id = dto.key.substringAfter("/works/"),
             title = dto.title,
-            author = dto.authorName.firstOrNull() ?: "Unknown Author",
+            author = dto.authorName?.firstOrNull() ?: "Unknown Author",
             coverId = dto.coverId,
             publishYear = dto.firstPublishYear,
-            language = mapLanguageCode(dto.language.toString())
+            language = mapLanguageCode(dto.language?.toString())
         )
     }
+
     fun mapDtoToDomainDetails(dto: BookDetailsDto): BookDetails {
+        val descriptionText = when (dto.description) {
+            is String -> dto.description
+            is LinkedTreeMap<*, *> -> {
+                (dto.description["value"] as? String) ?: "No description"
+            }
+            else -> "No description"
+        }
         return BookDetails(
-            description = dto.description?.value ?: "No description",
+            description = descriptionText,
             genres = dto.subjects
         )
     }
+
     private fun mapLanguageCode(code: String?): String {
         return when (code) {
             "eng" -> "English"
